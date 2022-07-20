a6fef47374c20f3974909ae267dc8cff4fff1f9c
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-20 21:39:52 +0200

    create Nameable class

--- person.rb
+++ person.rb
@@ -1,12 +1,20 @@
-require_relative './nameable.rb'
+# require_relative './nameable.rb'
+
+class Nameable
+    
+  def correct_name
+      raise NotImplementedError,"#{self.class} has not implemented method '#{__method__}'"
+      @name
+  end
+end
 
 # Person inherit from Nameable
-class Person<Nameable
+class Person < Nameable
   # getters and setters
   attr_reader :id
   attr_accessor :name, :age
   # contructor
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, name,parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -22,6 +30,10 @@ class Person<Nameable
   #  can use service method
   def can_use_service?
     @parent_permission || is_of_age?
+  end
+
+  def correct_name
+    @name
   end
 
   private :of_age?
