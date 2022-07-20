839422ec5bcaee4b2aee3596a1fec577038c8968
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-20 21:46:54 +0200

    add test for decorations

--- person.rb
+++ person.rb
@@ -69,3 +69,9 @@ class BaseDecorator < Nameable
   
   end
   
+    person = Person.new(22, 'maximilianus')
+      person.correct_name
+      capitalizedPerson = CapitalizeDecorator.new(person)
+      p capitalizedPerson.correct_name
+      capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+      p capitalizedTrimmedPerson.correct_name
