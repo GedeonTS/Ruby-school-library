6c761755d716a6510c91768986283f2e7e84da54
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-20 21:50:28 +0200

    remove nameable file

--- nameable.rb
+++
@@ -1,9 +0,0 @@
-class Nameable
-    initialize(name){
-        @name=name
-    }
-    def correct_name
-        raise NotImplementedError,"#{self.class} has not implemented method '#{__method__}'"
-        @name
-    end
-end
