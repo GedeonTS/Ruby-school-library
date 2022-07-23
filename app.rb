2fdb71464d7ffbb771cb003654f592481ec2192b
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-23 11:54:25 +0200

    Add my_menu method to class App

--- app.rb
+++ app.rb
@@ -12,5 +12,18 @@ class App
     @people = []
   end
 
+  def my_menu
+    puts "\n\nPlease choose an option by entering a number:"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts "7 - Exit\n\n"
+    gets.chomp
+  end
+
+
   
 end
