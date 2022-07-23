1722b5af6120a7763d8f58fdef7e17a69c69c91f
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-23 12:08:15 +0200

    Update parameters for class Teacher

--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,11 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(specialization, age, parent_permission: true, name: 'Unknown')
-    super(name, age, parent_permission)
+  def initialize(age, name, specialization)
+    super(age, name)
     @specialization = specialization
+    @age = age
+    @name = name
   end
 
   def can_use_service?
