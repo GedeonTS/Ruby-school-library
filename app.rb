ba4bafdd03675e0ec23290bded6d7f58c7233fa5
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-23 11:52:47 +0200

    Create class App

---
+++ app.rb
@@ -0,0 +1,16 @@
+require_relative 'classroom'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'rental'
+require_relative 'book'
+require_relative 'person'
+
+class App
+  def initialize
+    @my_books = []
+    @my_rentals = []
+    @people = []
+  end
+
+  
+end
